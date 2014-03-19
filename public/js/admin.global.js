d12d39b08d794039d228d22999b7ed39112e7cb1
vince <vince@vince-VirtualBox.(none)>
2014-03-19 18:46:07 +0800

    init the dev branch

--- public/js/admin.global.js
+++
@@ -1,9 +0,0 @@
-function resizeSidebar() {
-    var bodyHeight = $('body').height();
-    var windowHeight = $(window).height();
-    var screenHeight = bodyHeight > windowHeight ? bodyHeight : windowHeight;
-    var topHeight = $('.cd_top').height();
-    var sidebarHeight = screenHeight - topHeight - 2;
-    $('.cd_sidebar').height(sidebarHeight);	
-}
-
