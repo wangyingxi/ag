0f7aebf7005a401fa1af911f54c820626dde82b4
vince <vince@vince-VirtualBox.(none)>
2014-05-07 00:32:52 +0800

    1

--- application/modules/angel/controllers/ProductController.php
+++ application/modules/angel/controllers/ProductController.php
@@ -25,7 +25,7 @@ class Angel_ProductController extends Angel_Controller_Action {
             $this->view->model = $product;
             $this->view->currency = $this->bootstrap_options['currency'];
             $this->view->currency_symbol = $this->bootstrap_options['currency_symbol'];
-            
+
             // 将DBRef数组转换为json数组字符串输出到页面
 //            $photoJson = false;
 //            if ($product->photo) {
@@ -36,8 +36,34 @@ class Angel_ProductController extends Angel_Controller_Action {
 //                $photoJson = json_encode($ps);
 //            }
 //            $this->view->photoJson = $photoJson;
-            
-            
+        }
+    }
+
+    public function cartAction() {
+        if ($this->request->isPost()) {
+            $query = $this->request->getParam('query');
+            if ($query) {
+                $productModel = $this->getModel('product');
+                $qs = explode("|", $query);
+                $result = array();
+                foreach ($qs as $id) {
+                    $product = $productModel->getById($id);
+                    $pArr = array('id' => $id,
+                        'title' => $product->name,
+                        'location' => $product->location);
+                    $new_selling_price = array();
+                    foreach ($product->selling_price as $key => $val) {
+                        $new_selling_price[$key] = array('symbol' => $this->bootstrap_options['currency_symbol'][$key], 'amount' => $val);
+                    }
+                    $pArr['selling_price'] = $new_selling_price;
+                    if (count($product->photo) > 0) {
+                        $p0 = $product->photo[0];
+                        $pArr['photo'] = $this->view->photoImage($p0->name . $p0->type, 'small');
+                    }
+                    $result[] = $pArr;
+                }
+                var_dump($result);exit;
+            }
         }
     }
 
