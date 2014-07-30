0b70afd1f70b3dee6039ea3ecec75adb89db6141
vince <vince@vince-VirtualBox.(none)>
2014-07-30 18:13:41 +0800

    1

--- application/modules/angel/controllers/OrderController.php
+++ application/modules/angel/controllers/OrderController.php
@@ -2,15 +2,13 @@
 
 class Angel_OrderController extends Angel_Controller_Action {
 
-    protected $login_not_required = array('create');
+    protected $login_not_required = array('create','cart');
 
     public function init() {
         parent::init();
     }
 
     public function createAction() {
-
-
 
         if ($this->request->isPost()) {
 
@@ -19,7 +17,7 @@ class Angel_OrderController extends Angel_Controller_Action {
             if ($this->me) {
                 $user = $this->me->getUser();
             }
-            
+
             $currency = $_COOKIE['currency'];
             if (!$currency) {
                 $currency = key($this->bootstrap_options['currency']);
@@ -31,6 +29,40 @@ class Angel_OrderController extends Angel_Controller_Action {
 
     public function updateAction() {
         
+    }
+
+    public function cartAction() {
+        if ($this->request->isPost()) {
+            $query = $this->request->getParam('query');
+            $resource = array();
+            $code = 404;
+            if ($query) {
+                $productModel = $this->getModel('product');
+                $qs = explode("|", $query);
+                foreach ($qs as $id) {
+                    $product = $productModel->getById($id);
+                    $pArr = array('id' => $id,
+                        'title' => $product->title,
+                        'location' => $product->location);
+                    $new_selling_price = array();
+                    foreach ($product->selling_price as $key => $val) {
+                        $new_selling_price[$key] = array('symbol' => $this->bootstrap_options['currency_symbol'][$key], 'amount' => $val);
+                    }
+                    $pArr['selling_price'] = $new_selling_price;
+                    if (count($product->photo) > 0) {
+                        $p0 = $product->photo[0];
+                        $pArr['photo'] = $this->view->photoImage($p0->name . $p0->type, 'small');
+                    }
+                    $pArr['link'] = $this->view->url(array('id' => $id), 'product-view');
+                    $resource[] = $pArr;
+                }
+                $code = 200;
+            }
+            $this->_helper->json(array('data' => $resource,
+                'code' => $code));
+        } else {
+            $this->view->title = "Shopping Cart";
+        }
     }
 
 }
--- application/modules/angel/controllers/ProductController.php
+++ application/modules/angel/controllers/ProductController.php
@@ -3,8 +3,7 @@
 class Angel_ProductController extends Angel_Controller_Action {
 
     protected $login_not_required = array(
-        'view',
-        'cart'
+        'view'
     );
 
     public function init() {
@@ -37,48 +36,6 @@ class Angel_ProductController extends Angel_Controller_Action {
 //                $photoJson = json_encode($ps);
 //            }
 //            $this->view->photoJson = $photoJson;
-        }
-    }
-
-    public function cartAction() {
-        if ($this->request->isPost()) {
-            $query = $this->request->getParam('query');
-            $resource = array();
-            $code = 404;
-            if ($query) {
-                $productModel = $this->getModel('product');
-                $qs = explode("|", $query);
-                foreach ($qs as $id) {
-                    $product = $productModel->getById($id);
-                    $pArr = array('id' => $id,
-                        'title' => $product->title,
-                        'location' => $product->location);
-                    $new_selling_price = array();
-                    foreach ($product->selling_price as $key => $val) {
-                        $new_selling_price[$key] = array('symbol' => $this->bootstrap_options['currency_symbol'][$key], 'amount' => $val);
-                    }
-                    $pArr['selling_price'] = $new_selling_price;
-                    if (count($product->photo) > 0) {
-                        $p0 = $product->photo[0];
-                        $pArr['photo'] = $this->view->photoImage($p0->name . $p0->type, 'small');
-                    }
-                    $pArr['link'] = $this->view->url(array('id' => $id), 'product-view');
-                    $resource[] = $pArr;
-                }
-                $code = 200;
-            }
-            $this->_helper->json(array('data' => $resource,
-                'code' => $code));
-        } else {
-            $this->view->title = "Shopping Cart";
-        }
-    }
-
-    public function checkoutAction() {
-        if ($this->request->isPost()) {
-            
-        } else {
-            $this->view->title = "Checkout Order";
         }
     }
 
